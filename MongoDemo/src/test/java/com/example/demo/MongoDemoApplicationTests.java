12012af7bbf11a742bcd380b0b278215b76ee004
amardwells <154550212+amardwells@users.noreply.github.com>
2023-12-21 16:10:58 -0600

    Update MongoDemoApplicationTests.java

--- MongoDemo/src/test/java/com/example/demo/MongoDemoApplicationTests.java
+++ MongoDemo/src/test/java/com/example/demo/MongoDemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MongoDemoApplicationTests {
 
+	//Hey this is remote commit from github
 	@Test
 	void contextLoads() {
 	}
